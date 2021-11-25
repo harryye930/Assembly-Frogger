23eb285b17dcf1865a4e783a94e52f6f743fffe6
Runlong (Harry) Ye <89664586+harryye930@users.noreply.github.com>
2021-11-25 14:13:56 -0500

    update drawing board using function

--- frogger.asm
+++ frogger.asm
@@ -22,26 +22,7 @@
 #########	Start of program	##########
 gameStart:
 	lw $t0, displayAddress # $t0 stores the base address for display
-	la $t1, 0($t0)
-	la $t8, 768($t0) # finish line green before 768 offset
-	lw $t9, grassGreen # load grassGreen
-	jal graphRectangle
-	la $t8, 1792($t0) # river blue before 1792 offset
-	lw $t9, riveBlue # load riveBlue
-	jal graphRectangle
-	la $t8, 2304($t0) # middle purple before 2304 offset
-	lw $t9, middleRestPurple # load middleRestPurple
-	jal graphRectangle
-	la $t8, 3328($t0) # road black before 3328 offset
-	lw $t9, roadBlack # load roadBlack
-	jal graphRectangle
-	la $t8, 4096($t0) # start line green before 4096 offset
-	lw $t9, grassGreen # load grassGreen
-	jal graphRectangle
-	
-	li $v0, 32
- 	li $a0, 1000
- 	syscall
+	jal graphGameBoard # call grpah game board function
 	
 	la $t7, 3636($t0) # load the initial offset of frog in t7
 	lw $t8, frogGreen # store frogGreen to t8
@@ -52,7 +33,7 @@ gameStart:
  	syscall
  	
  	
-whileInRange:
+whileInRange:  # WHile looping if frog don't hit the finish line
 	la $t6, 128($t0) # load end condition to stop while loop
 	blt $t7, $t6, exitWhileInRange  # exit while loop if t7 (offset of frog) < 128 --> frog have reach other side
 	#bge $t7, $t6, checkUserInput # check user input if not reached other side
@@ -67,23 +48,7 @@ checkW:
 	j checkA
 
 respondToW:
- 	la $t1, 0($t0) # start regraph game board
-	la $t8, 768($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle
-	la $t8, 1792($t0) 
-	lw $t9, riveBlue 
-	jal graphRectangle
-	la $t8, 2304($t0) 
-	lw $t9, middleRestPurple 
-	jal graphRectangle
-	la $t8, 3328($t0) 
-	lw $t9, roadBlack 
-	jal graphRectangle
-	la $t8, 4096($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle # end regraph game board
-	
+ 	jal graphGameBoard
 	
 	addi $t7, $t7, -128  # up 1 unit
 	lw $t8, frogGreen # store frogGreen to t8
@@ -96,23 +61,7 @@ checkA:
 	j checkS
 
 respondToA:
- 	la $t1, 0($t0) # start regraph game board
-	la $t8, 768($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle
-	la $t8, 1792($t0) 
-	lw $t9, riveBlue 
-	jal graphRectangle
-	la $t8, 2304($t0) 
-	lw $t9, middleRestPurple 
-	jal graphRectangle
-	la $t8, 3328($t0) 
-	lw $t9, roadBlack 
-	jal graphRectangle
-	la $t8, 4096($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle # end regraph game board
-
+ 	jal graphGameBoard
 	
 	addi $t7, $t7, -4  # left 1 unit
 	lw $t8, frogGreen # store frogGreen to t8
@@ -126,22 +75,7 @@ checkS:
 	j checkD
 
 respondToS:
- 	la $t1, 0($t0) # start regraph game board
-	la $t8, 768($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle
-	la $t8, 1792($t0) 
-	lw $t9, riveBlue 
-	jal graphRectangle
-	la $t8, 2304($t0) 
-	lw $t9, middleRestPurple 
-	jal graphRectangle
-	la $t8, 3328($t0) 
-	lw $t9, roadBlack 
-	jal graphRectangle
-	la $t8, 4096($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle # end regraph game board
+ 	jal graphGameBoard
 	
 	
 	addi $t7, $t7, 128  # down 1 unit
@@ -154,22 +88,7 @@ checkD:
 	beq $t4, 0x64, respondToD
 
 respondToD:
- 	la $t1, 0($t0) # start regraph game board
-	la $t8, 768($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle
-	la $t8, 1792($t0) 
-	lw $t9, riveBlue 
-	jal graphRectangle
-	la $t8, 2304($t0) 
-	lw $t9, middleRestPurple 
-	jal graphRectangle
-	la $t8, 3328($t0) 
-	lw $t9, roadBlack 
-	jal graphRectangle
-	la $t8, 4096($t0) 
-	lw $t9, grassGreen 
-	jal graphRectangle # end regraph game board
+ 	jal graphGameBoard
 
 	
 	addi $t7, $t7, +4  # right 1 unit
@@ -230,27 +149,26 @@ graphFrog:
 	jr $ra
 	
 graphGameBoard:
-	la $t7, 0($t0)  # init iterator to 0
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	la $t1, 0($t0)
 	la $t8, 768($t0) # finish line green before 768 offset
 	lw $t9, grassGreen # load grassGreen
 	jal graphRectangle
-	
 	la $t8, 1792($t0) # river blue before 1792 offset
 	lw $t9, riveBlue # load riveBlue
 	jal graphRectangle
-	
 	la $t8, 2304($t0) # middle purple before 2304 offset
 	lw $t9, middleRestPurple # load middleRestPurple
 	jal graphRectangle
-	
 	la $t8, 3328($t0) # road black before 3328 offset
 	lw $t9, roadBlack # load roadBlack
 	jal graphRectangle
-	
 	la $t8, 4096($t0) # start line green before 4096 offset
 	lw $t9, grassGreen # load grassGreen
 	jal graphRectangle
-
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 	jr $ra
 	
 graphDeadRespawnAnima:
